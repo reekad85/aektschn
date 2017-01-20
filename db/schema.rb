9124700dfa1d486c7eaff5ed32598532575e8639
reekad85 <r.kadhum@gmail.com>
2017-01-20 15:07:25 +0100

    4.2 added prices

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170119141913) do
+ActiveRecord::Schema.define(version: 20170120135828) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20170119141913) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "colour"
+    t.integer  "price"
   end
 
 end
