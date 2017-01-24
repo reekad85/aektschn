3783573960ab28342a3135a4c452c9201a458411
reekad85 <r.kadhum@gmail.com>
2017-01-24 20:32:27 +0100

    4.2 fixed products_controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,8 +6,12 @@ class ProductsController < ApplicationController
   # GET /products.json
   def index
     if params[:q]
-      search_term = params[:q]
-      @products = Product.where("name LIKE ?", "%#{search_term}%")
+      if(Rails.env.development?)
+        @products = Product.where("name LIKE ?", "%#{search_term}%")
+      else
+        # use ilike for case insensitivity on postres
+        @products = Product.where("name ilike ?", "%#{search_term}%")
+      end
     else
       @products = Product.all
     end
