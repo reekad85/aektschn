8b4e29ca46e4922c3e7dae73284e9956aef36677
reekad85 <r.kadhum@gmail.com>
2017-01-24 20:54:41 +0100

    minor fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-<<<<<<< HEAD
- 
+  before_filter :authenticate_user!
   load_and_authorize_resource
-=======
->>>>>>> master
 
   # GET /users
   # GET /users.json
