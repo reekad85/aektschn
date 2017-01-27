ef8ab3700272a71d6db3e2380eb8b841488a70bc
reekad85 <r.kadhum@gmail.com>
2017-01-27 14:15:12 +0100

    update usermailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
 	after_create :send_user_mail
-  def send_user_mail
-    UserMailer.send_new_user_message(self).deliver
+  	def send_user_mail
+    	UserMailer.send_new_user_message(self).deliver
+  	end
   end
