a952a9e4f6c2f4de18822a9be2d12c654336cde8
reekad85 <r.kadhum@gmail.com>
2017-01-30 11:50:03 +0100

    fix welcome mail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,10 +26,11 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params)
+    @user = User.new(params[:user])
 
     respond_to do |format|
       if @user.save
+        UserMailer.welcome_email(@user).deliver_later
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,9 +8,10 @@ class UserMailer < ApplicationMailer
         :subject => "A new contact form message from #{name}")
   end
 
-  def welcome(user)
-    @appname = "Specialtea"
-    mail( :to => user.email,
-          :subject => "Welcome to #{@appname}!")
+  def welcome_email(user)
+    @user = user
+    @url  = 'http://localhost:3000/users/sign_up'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
 end
+
