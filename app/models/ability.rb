78a029c95f4fffbe99dcd486cc1e01b570776f79
reekad85 <r.kadhum@gmail.com>
2017-01-30 20:11:35 +0100

    5.5 fix cancancan user ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,14 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
+    can :create, Comment
+    can :read, Comment
+    can :read, Product
+
+    if user.admin?
+      can :destroy, Comment
+      can :manage, Product
+    end
   end
 
 end
