cd7d836b2f96b96d5d6a08d419fdf0df88d6880f
reekad85 <r.kadhum@gmail.com>
2017-01-30 18:14:22 +0100

    fix welcome mail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,6 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        puts "******************** NEW USER ****************************"
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
