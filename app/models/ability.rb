257a575493d572170e56a7619974923497ca6a69
reekad85 <r.kadhum@gmail.com>
2017-01-31 15:58:34 +0100

    5.5 updating abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,7 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
-    cannot :manage, Product, id: user.id
+    cannot :create, Product
 	end
 
 end
