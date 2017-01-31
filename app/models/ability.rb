dc1e479a6533b722be20f6b7eb8aab2e0ef64da3
reekad85 <r.kadhum@gmail.com>
2017-01-31 14:56:55 +0100

    5.5 fix user authentication

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,14 +4,13 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
-    can :create, Comment
-    can :read, Comment
-    can :read, Product
 
     if user.admin?
-      can :destroy, Comment
-      can :manage, Product
-    end
+     can :destroy, Comment
+     can :manage, Product do |product|
+  		product.user_id == user.id
+	end
   end
 
 end
+
