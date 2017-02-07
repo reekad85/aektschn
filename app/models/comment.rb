396b5b5075a949319083d9512d82f78408d13cba
reekad85 <r.kadhum@gmail.com>
2017-02-07 18:31:34 +0100

    5.7 comment fix

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,11 +2,12 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :product
 
+  scope :rating_desc, -> { order('comments.rating DESC') }
+  scope :rating_asc, -> { order(rating: :asc) }
+
   validates :body, presence: true
   validates :user, presence: true
   validates :product, presence: true
   validates :rating, numericality: { only_integer: true }
-
-  scope :rating_desc, -> { order(rating: :desc) }
-  scope :rating_asc, -> { order(rating: :asc) }
+  
 end
