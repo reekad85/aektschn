de9e475625191616be22f76e6b00fbf0cd669ee4
reekad85 <r.kadhum@gmail.com>
2017-02-08 20:51:48 +0100

    6.3 functional test

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,10 +3,6 @@ class Product < ActiveRecord::Base
   has_many :comments
 
   validates :name, presence: true
-  validates :body, presence: true
-  validates :user, presence: true
-  validates :product, presence: true
-  validates :rating, numericality: { only_integer: true}
 
   def highest_rating_comment
   	comments.rating_desc.first
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe UsersController, :type => :controller do
 
-	#let(:user_2) { User.create!(email: 'reem@example.com', password: '0987654321') }
-	let(:user) { User.create!(email: 'reem@example.com', password: '0987654321') }
+	let(:user_2) { User.create!(email: 'rimu@example.com', password: '0987654321') }
+	let(:user) { User.create!(email: 'rumi@example.com', password: '0987654321') }
 	#@user = FactoryGirl.create(:user)
 	
 
@@ -18,13 +18,19 @@ describe UsersController, :type => :controller do
 				expect(response).to have_http_status(200)
 				expect(assigns(:user)).to eq user
 			end
+
+			it 'does not load user_2 show' do
+				get :show, id: user_2.id
+				expect(response).to have_http_status(302)
+				expect(response).to redirect_to(root_path)
+			end
 		end
 
 		context 'No User is logged in' do
 			it 'redirects to login' do
-        get :show, id: user.id
-        expect(response).to redirect_to(new_user_session_path)
-      end
-    end
+        		get :show, id: user.id
+        		expect(response).to redirect_to(new_user_session_path)
+      		end
+    	end
 	end
 end
