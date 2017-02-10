fcea1547b22f252ccc2a64eabd2bd2d70bd6bd8e
reekad85 <r.kadhum@gmail.com>
2017-02-10 16:36:16 +0100

    commenting force ssl

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,9 @@ module Aektschn
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.force_ssl = true
+    
+    # This would force to use a more secure connection
+    #config.force_ssl = true
   end
 end
 
