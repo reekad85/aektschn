f0273ce1b70c5527bca25c1d11b2d1d781aae9a2
reekad85 <r.kadhum@gmail.com>
2017-02-15 14:29:46 +0100

    6.10 ajax

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,13 +1,18 @@
-$(document).on('turbolinks:load', function(){
-	$('.img-zoom').elevateZoom({
+var refreshRating = function() {
+    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+    $('.rated').raty({ path: '/assets',
+        readOnly: true,
+        score: function() {
+            return $(this).attr('data-score');
+        }
+    });
+};
+
+$(document).on('turbolinks:load ajaxSuccess', function() {
+    refreshRating();
+
+    $('.img-zoom').elevateZoom({
 		zoomType: "lens",
   		lensShape: "round",
   		lensSize: 200});
-    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-    $('.rated').raty({ path: '/assets',
-      readOnly: true,
-      score: function() {
-        return $(this).attr('data-score');
-      }
-    });
 });
