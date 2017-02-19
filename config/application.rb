08f1293216962201ecff186536b37eafc79e2a54
reekad85 <r.kadhum@gmail.com>
2017-02-19 15:45:03 +0100

    update to rails 5

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
-require "rails"
+require 'rails/all'
+
 # Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
@@ -49,4 +50,4 @@ end
         #FactoryGirl.find_definitions
     #end
 
-#end  
+#end 
